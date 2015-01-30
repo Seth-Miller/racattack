cb0b80e625654949f9993868f48127b0ca964068
root <root@collabn1.racattack>
2015-01-29 22:14:59 -0600

    changed 5-storage.sh to be able to run multiple times added database response file

--- config/setup/5-storage.sh
+++ config/setup/5-storage.sh
@@ -4,9 +4,15 @@ sed -i -e '$aoptions=-g' /etc/scsi_id.config
 
 i=1
 cmd="/sbin/scsi_id -g -u -d"
+awkscript='BEGIN {FS = ":"; count = 0}; $1 == "1" {count = count+1}; END {print count}'
 > /etc/udev/rules.d/99-oracle-asmdevices.rules
-for disk in sdb sdc sdd sde; do 
-	if [ "$(hostname -s)" = "collabn1" ]; then
+for disk in sdb sdc sdd sde sdf sdg sdh; do 
+	if [ "$(hostname -s)" != "collabn1" ]; then
+		echo "This is not collabn1, skipping partition creation for disk /dev/$disk"
+	elif [ "$(parted -m --script /dev/$disk print | awk "$awkscript")" != "0" ]; then
+		echo "The partition for disk /dev/$disk has already been created, skipping partition creation"
+	else
+		parted --script /dev/$disk mklabel msdos
 		parted --script /dev/$disk mkpart primary 0% 100%
 	fi
 	partprobe /dev/${disk}
@@ -15,5 +21,9 @@ for disk in sdb sdc sdd sde; do
 	'OWNER="oracle", GROUP="dba", MODE="0660"' >> /etc/udev/rules.d/99-oracle-asmdevices.rules
 	let i++ 
 done
+
+echo
+cat /etc/udev/rules.d/99-oracle-asmdevices.rules
+echo
 
 start_udev
